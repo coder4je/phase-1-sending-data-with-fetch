8451f7257c5b2d0bc5b59deb85d40c7969d2f26c
Taemin Kim <taeminkimny@gmail.com>
2022-05-30 16:08:25 -0400

    commit tree 6728fcbdf870772f256d9112ce37db73ade0a70e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData (name, email) {
+    const personalInfo = {
+        "name" : "email",         
+    }
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'Application/json',
+            'Accept' : 'Application/json',
+        },
+        body: JSON.stringify(personalInfo)
+    })
+    .then (res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch (error => {
+        alert("Something Wrong")
+    });
+}
+
+
+submitData()
